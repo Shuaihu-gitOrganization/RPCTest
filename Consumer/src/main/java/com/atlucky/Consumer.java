3c189ad22703fe784c610d2517a2cefe14a210e0
shuaihu.zhang <shuaihu.zhang@9fzt.com.cn>
2023-08-30 15:37:39 +0800

    test[]:测试请求连通性

--- Consumer/src/main/java/com/atlucky/Consumer.java
+++ Consumer/src/main/java/com/atlucky/Consumer.java
@@ -1,6 +1,9 @@
 package com.atlucky;
 
+import com.atlucky.common.Invocation;
+import com.atlucky.protocol.http.HttpClient;
 import com.common.HelloService;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @Date 2023/8/30 13:30
@@ -9,8 +12,16 @@ import com.common.HelloService;
  **/
 public class Consumer {
     public static void main(String[] args) {
-        HelloService helloService= null ;
-        String sayHello = helloService.sayHello("atLucky");
+        /*HelloService helloService= null ;
+        String sayHello = helloService.sayHello("atLucky");*/
+
+        Invocation sayHello = new Invocation(HelloService.class.getName(),
+                "sayHello",
+                new Class[]{String.class},
+                new Object[]{"张三"});
+        HttpClient httpClient = new HttpClient();
+        String result = httpClient.send("localhost", 9999, sayHello);
+        System.out.println(result);
 
     }
 }
