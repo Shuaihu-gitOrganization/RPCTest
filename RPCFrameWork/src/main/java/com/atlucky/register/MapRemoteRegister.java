96a7d76d1a0cddb82a46cde09ed110fa03dd8d38
shuaihu.zhang <shuaihu.zhang@9fzt.com.cn>
2023-08-30 17:12:40 +0800

    add[]:新增本地缓存服务资源，服务容错实现

--- RPCFrameWork/src/main/java/com/atlucky/register/MapRemoteRegister.java
+++ RPCFrameWork/src/main/java/com/atlucky/register/MapRemoteRegister.java
@@ -2,6 +2,7 @@ package com.atlucky.register;
 
 import com.atlucky.common.URL;
 
+import java.io.*;
 import java.util.*;
 
 /**
@@ -20,10 +21,41 @@ public class MapRemoteRegister {
         }
         urls.add(url);
         map.put(interfaceName,urls);
+        saveFile();
 
     }
     public static List<URL> getRemoteRegister(String interfaceName){
+        map = getFile();
         return map.get(interfaceName);
+    }
+
+
+    public static void  saveFile(){
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("/temp.txt");
+            ObjectOutputStream stream = new ObjectOutputStream(fileOutputStream) ;
+                stream.writeObject(map);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public static Map<String,List<URL>>  getFile(){
+        try {
+            FileInputStream fileInputStream = new FileInputStream("/temp.txt");
+            ObjectInputStream stream = new ObjectInputStream(fileInputStream) ;
+            return (Map<String,List<URL>>) stream.readObject();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
