8d8b7798d0a05f4d43ac3d8e77746fa9db6fccb9
shuaihu.zhang <shuaihu.zhang@9fzt.com.cn>
2023-08-30 14:12:38 +0800

    Tomcat内嵌服务启动

---
+++ RPCFrameWork/src/main/java/com/atlucky/protocol/http/HttpServer.java
@@ -0,0 +1,58 @@
+package com.atlucky.protocol.http;
+
+import jdk.swing.interop.DispatcherWrapper;
+import org.apache.catalina.*;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.core.StandardEngine;
+import org.apache.catalina.core.StandardHost;
+import org.apache.catalina.startup.Tomcat;
+
+/**
+ * @Date 2023/8/30 13:50
+ * @Author XiaoHu
+ * @Description
+ **/
+public class HttpServer {
+    public void start(String hostname,Integer port){
+        //读取用户的配置
+        Tomcat tomcat = new Tomcat();
+
+        Server server = tomcat.getServer();
+        Service service = server.findService("Tomcat");
+
+        Connector connector = new Connector();
+        connector.setPort(port);
+
+        Engine engine = new StandardEngine();
+        engine.setDefaultHost(hostname);
+
+        Host standardHost = new StandardHost();
+        standardHost.setName(hostname);
+
+        String contextPath="";
+
+        Context context = new StandardContext();
+         context.setPath(contextPath);
+         context.addLifecycleListener(new Tomcat.FixContextListener());
+
+         standardHost.addChild(context);
+
+         engine.addChild(standardHost);
+
+         service.setContainer(engine);
+         service.addConnector(connector);
+
+         //tomcat.addServlet(contextPath,"disPatcher",new DispatcherServlet())
+
+        try {
+            tomcat.start();
+            tomcat.getServer().await();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
