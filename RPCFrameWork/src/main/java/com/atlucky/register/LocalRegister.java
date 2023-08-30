89e1798911871b4fc9df2c7b6f69c679744e1c10
shuaihu.zhang <shuaihu.zhang@9fzt.com.cn>
2023-08-30 15:13:52 +0800

    add[]:增加本地注册机制

---
+++ RPCFrameWork/src/main/java/com/atlucky/register/LocalRegister.java
@@ -0,0 +1,23 @@
+package com.atlucky.register;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Date 2023/8/30 14:53
+ * @Author XiaoHu
+ * @Description
+ **/
+public class LocalRegister {
+    public static Map<String,Class> map=new HashMap<String,Class>();
+
+    public static void register(String interfaceName,String version,Class implClass){
+        map.put(interfaceName+version,implClass);
+
+    }
+    public static Class getRegister(String interfaceName,String version){
+        return map.get(interfaceName+version);
+    }
+
+
+}
