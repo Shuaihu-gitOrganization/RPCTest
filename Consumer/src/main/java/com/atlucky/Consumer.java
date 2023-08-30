0b3e369826dde9b8df8a7d4f5b2f2edb143ba4e2
shuaihu.zhang <shuaihu.zhang@9fzt.com.cn>
2023-08-30 16:06:21 +0800

    test[]:模拟测试远程调用方法

--- Consumer/src/main/java/com/atlucky/Consumer.java
+++ Consumer/src/main/java/com/atlucky/Consumer.java
@@ -2,6 +2,7 @@ package com.atlucky;
 
 import com.atlucky.common.Invocation;
 import com.atlucky.protocol.http.HttpClient;
+import com.atlucky.proxy.ProxyFactory;
 import com.common.HelloService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,16 +13,10 @@ import lombok.extern.slf4j.Slf4j;
  **/
 public class Consumer {
     public static void main(String[] args) {
-        /*HelloService helloService= null ;
-        String sayHello = helloService.sayHello("atLucky");*/
+        HelloService helloService= ProxyFactory.getProxy(HelloService.class);
+        String sayHello = helloService.sayHello("atLucky");
 
-        Invocation sayHello = new Invocation(HelloService.class.getName(),
-                "sayHello",
-                new Class[]{String.class},
-                new Object[]{"张三"});
-        HttpClient httpClient = new HttpClient();
-        String result = httpClient.send("localhost", 9999, sayHello);
-        System.out.println(result);
+
 
     }
 }
--- Provider/src/main/java/service/implement/HelloServiceImpl.java
+++ Provider/src/main/java/service/implement/HelloServiceImpl.java
@@ -10,6 +10,6 @@ import com.common.HelloService;
 public class HelloServiceImpl implements HelloService {
     @Override
     public String sayHello(String name) {
-        return "Hello"+name;
+        return "Hello :"+name;
     }
 }
