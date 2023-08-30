d805fcd9f0e41cb75c379c0d11b35b195decaf47
shuaihu.zhang <shuaihu.zhang@9fzt.com.cn>
2023-08-30 15:37:14 +0800

    add[]:新增HttpClient

---
+++ RPCFrameWork/src/main/java/com/atlucky/protocol/http/HttpClient.java
@@ -0,0 +1,46 @@
+package com.atlucky.protocol.http;
+
+import com.atlucky.common.Invocation;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * @Date 2023/8/30 15:19
+ * @Author XiaoHu
+ * @Description
+ **/
+public class HttpClient {
+    public String send(String hostName, Integer port, Invocation invocation){
+        //用户配置
+
+        HttpURLConnection urlConnection;
+        try {
+            URL url = new URL("http", hostName, port, "/");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoOutput(true);
+
+            //配置
+            OutputStream outputStream = urlConnection.getOutputStream();
+            ObjectOutputStream stream = new ObjectOutputStream(outputStream);
+            stream.writeObject(invocation);
+
+            stream.flush();
+            stream.close();
+            InputStream inputStream = urlConnection.getInputStream();
+            String result = IOUtils.toString(inputStream);
+           return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
